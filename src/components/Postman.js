798e755d971c150d49adb510ba4b644990dea508
Fulton Zink <joeyzink@gmail.com>
2019-06-18 14:48:11 -0700

    Add form with text field and button.

--- src/components/Postman.js
+++ src/components/Postman.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 
+import '../styles.scss';
+
 export default class Postman extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      apiUrl: '',
+      apiResult: 'JSON Data',
+    };
   }
 
+  handleChange = () => {};
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
-    return <div className="Postman">This is Postman.</div>;
+    const { apiUrl, apiResult } = this.state;
+    return (
+      <div className="Postman">
+        <h1>Postman</h1>
+        <form>
+          <label htmlFor="apiurl">
+            API URL:
+            <input
+              id="apiurl"
+              className="form-input"
+              type="text"
+              value={apiUrl}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="button" onClick={this.handleSubmit}>
+            Submit
+          </button>
+          <div>{apiResult}</div>
+        </form>
+      </div>
+    );
   }
 }

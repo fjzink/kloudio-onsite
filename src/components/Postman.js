abd15f9fe86e70ad7e9ac4a9088847fa472a0c7e
Fulton Zink <joeyzink@gmail.com>
2019-06-18 14:22:34 -0700

    Add Postman component.

---
+++ src/components/Postman.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Postman extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="Postman">This is Postman.</div>;
+  }
+}
